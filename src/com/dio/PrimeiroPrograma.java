dff31e311b93b167b303d05b7caeb3b3207b7b8f
matoslopes23 <matoslopes23@gmail.com>
2021-08-20 08:50:25 -0400

    Primeiro projeto java

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,21 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
